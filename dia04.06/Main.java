6a519ddf20166ba24e8e89e2f6be16d5699adb0e
luiz <luizhenriquedossantos1505@gmail.com>
2025-06-04 09:07:46 -0300

    Create Main.java

---
+++ dia04.06/Main.java
@@ -0,0 +1,40 @@
+package luiz_java;
+
+import java.util.Scanner;
+
+public class Main {
+public static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int opcao = 0;
+
+            System.out.println("Menu: \n 1 - Cadastrar Paciente \n 2 - Cadastrar Médico \n 3 - Registrar Consulta \n "
+            		+ "4 - Ver Consultas de Paciente \n 5 - Sair \n Escolha: ");
+
+            opcao = entrada.nextInt();
+            // nextLine necessário para que o próximo input não seja ignorado
+            entrada.nextLine();
+
+            switch (opcao) {
+            case 1:
+            	Paciente.cadastrarPaciente();
+            	break;
+            
+            case 2:
+                Medico.cadastrarMedico();
+                break;
+                
+            case 3:
+            	Hospital.registrarConsulta();
+                break;
+                
+            case 4:
+            	Hospital.consultasPaciente();
+            	break;
+            	
+            case 5:
+                System.out.println("Sistema encerrado.");
+                System.exit(0);
+            }
+       }
+   }
