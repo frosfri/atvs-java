37d7989cae1fd3c79543040e0b6c1ce9cf3c5de6
LUIZ <luizhenriquedossantos1505@gmail.com>
2025-07-24 07:32:57 -0300

    Update impar.java

--- 23.07/impar.java
+++ 23.07/impar.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Aluno
- */
 public class impar {
+public class Atv6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
     
 }
