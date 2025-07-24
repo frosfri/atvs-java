380f1a30fcddee2d7ed65b0539798ece4ca178b1
LUIZ <luizhenriquedossantos1505@gmail.com>
2025-07-24 07:31:20 -0300

    Update contagem.java

--- 23.07/contagem.java
+++ 23.07/contagem.java
@@ -1,3 +1,14 @@
 public class contagem {
-    
+    public class SomaSimples {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            soma += i;
+        }
+
+        System.out.println("Soma de 1 a 100: " + soma);
+    }
+}
+
 }
