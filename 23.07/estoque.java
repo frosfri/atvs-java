c2c8fc3f0a26417679b3fa6d4ec85d41496217fd
LUIZ <luizhenriquedossantos1505@gmail.com>
2025-07-24 07:31:54 -0300

    Update estoque.java

--- 23.07/estoque.java
+++ 23.07/estoque.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Aluno
- */
 public class estoque {
     
+    public static void main(String[] args) {
+        int[] estoque = {12, 8, 30, 5, 22, 40, 18};
+
+        int maior = estoque[0];
+        for (int i = 1; i < estoque.length; i++) {
+            if (estoque[i] > maior) {
+                maior = estoque[i];
+            }
+        }
+
+        System.out.println("Maior quantidade no estoque: " + maior);
+    }
 }
+
